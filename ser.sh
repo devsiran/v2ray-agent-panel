b55f7657f745bed7b1c98ef8b7ac1e4ce0061d85
devsiran <devsiran@gmail.com>
2022-12-07 13:20:13 +0330

    python update 2

---
+++ ser.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+red='\033[0;31m'
+green='\033[0;32m'
+yellow='\033[0;33m'
+plain='\033[0m'
+
+cur_dir=$(pwd)
+
+# check root
+[[ $EUID -ne 0 ]] && echo -e "${red}ERROR：${plain} The root user must be used to run this script!\n" && exit 1
+
+# check os
+if [[ -f /etc/redhat-release ]]; then
+    release="centos"
+elif cat /etc/issue | grep -Eqi "debian"; then
+    release="debian"
+elif cat /etc/issue | grep -Eqi "ubuntu"; then
+    release="ubuntu"
+elif cat /etc/issue | grep -Eqi "centos|red hat|redhat"; then
+    release="centos"
+elif cat /proc/version | grep -Eqi "debian"; then
+    release="debian"
+elif cat /proc/version | grep -Eqi "ubuntu"; then
+    release="ubuntu"
+elif cat /proc/version | grep -Eqi "centos|red hat|redhat"; then
+    release="centos"
+else
+    echo -e "${red}System version not detected!${plain}\n" && exit 1
+fi
+
+if [[ x"${release}" == x"centos" ]]; then
+    yum install python3 pip3 -y
+else
+    apt install python3 pip3 -y
+fi
+
+pip3 install requests 
+
+echo -e "Installed Successful"
